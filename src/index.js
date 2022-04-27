886351eeb0641fb0d0693c3cf17427dd6e51b8d4
eliyya <tecno_eli@hotmail.com>
2022-04-27 10:00:34 -0500

    events

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ config()
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS
     ]
 })
 
 client.login()
+
+client.once('ready', (client) => {
+    console.log(client.user.username + ' Ready!!!')
+})
+
+client.on('messageCreate', (message) => {
+    console.log(message.content)
+})
+
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+    if (oldMember.displayName !== newMember.displayName)
+        console.log(oldMember.displayName + ' changed their name to ' + newMember.displayName)
+})
